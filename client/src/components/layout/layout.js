61761f19420c873104b2db878e5f2542b3c34c1e
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2022-03-27 15:42:24 -0500

    Setup sign up and sign in

---
+++ client/src/components/layout/layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+
+export default function Layout(props) {
+    return (
+        <>
+           <Container>
+              {props.children}
+           </Container>
+        </>
+    );
+}
--- client/src/pages/signUp.js
+++ client/src/pages/signUp.js
@@ -1,11 +1,42 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Layout from '../components/layout/layout';
 
 const SignUpPage = () => {
   return (
     <>
-      <h1>Sign Up Page</h1>
-      <Button>Sign up</Button>
+     <Layout>
+        <h1>Sign Up Page</h1>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            
+            <Form.Label>Full Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter full name" />
+          </Form.Group>
+
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Sign up
+          </Button>
+        </Form>
+     </Layout>
     </>
   )
 }
