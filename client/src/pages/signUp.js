350187edc4faa8cc098f45820b5f3e06d1c2ca2b
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2022-03-27 15:46:19 -0500

    Remove unused button

--- client/src/pages/signUp.js
+++ client/src/pages/signUp.js
@@ -29,9 +29,7 @@ const SignUpPage = () => {
             <Form.Label>Password</Form.Label>
             <Form.Control type="password" placeholder="Password" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
+         
           <Button variant="primary" type="submit">
             Sign up
           </Button>
