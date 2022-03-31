dffe25b4743bc7e9f1110fa1ced2352e6879efdb
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2022-03-31 13:31:30 -0500

    Update notfound.js

--- client/src/pages/notfound.js
+++ client/src/pages/notfound.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Button from 'react-bootstrap/esm/Button';
+import Container from 'react-bootstrap/esm/Container';
 
 const NotFoundPage = () => {
     return (<>
-    <h1>404 Page not found</h1>
-    <Button href="/">Go home</Button>
+        <Container>
+            <h1>404 Page not found</h1>
+            <Button href="/">Go home</Button>
+        </Container>
     </>);
 
 }
