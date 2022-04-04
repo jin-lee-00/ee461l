2bab0b7cbe8398e070a65ba7a2ee3657a4eef373
Noah Zamarripa <57299625+NoahZ13@users.noreply.github.com>
2022-04-04 15:04:08 -0500

    fixed some mongo stuff and added heroku comments

--- server/app.py
+++ server/app.py
@@ -6,6 +6,7 @@ import os
 # from user.models import User
 
 app = Flask(__name__)
+# app = Flask(__name__, , static_folder='./build', static_url_path='/')
 CORS(app)
 app.secret_key = "temp_secret_key"
 
@@ -13,13 +14,15 @@ app.secret_key = "temp_secret_key"
 # MONGO DB STUFFS
 ##########################################################################################
 
-# connect to mongoDb client cluster0
+# # connect to mongoDb client cluster0
 client = pymongo.MongoClient("localhost", 27017) # local db for development
 
+data = client.data
+
 # get collections
-db_users = client.users
-db_HWSet = client.HWSet
-db_projects = client.projects
+db_users = data.users
+db_HWSet = data.HWSet
+db_projects = data.projects
 
 ##########################################################################################
 # FLASK API FUNCTIONS
@@ -39,7 +42,7 @@ def signup():
   # Check user is not in db_users
   if user == None:
   # Add user to db_users
-    db_users.insertOne(
+    db_users.insert_one(
         { "name": name,
           "email": email,
           "password": password,
@@ -53,13 +56,14 @@ def signin():
   email = request_data['email']
   user = db_users.find({},{ "email": email})
   # Check user is in db_users
-  if (user != None):
+  # if (user != None):
     # TODO: Authenticate (jwt?)
 
   return request_data
 
 @app.route("/")
 def home():
+    # return send_from_directory(app.static_folder, "index.html")
     return "Hello World"
 
 
@@ -71,3 +75,4 @@ client.close()
 
 if __name__ == "__main__":
     app.run(debug=True)
+    # app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
