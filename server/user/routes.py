f0b695ac4ccabacef085e37583f74bc34cd9e513
Jin Lee <jinolemo@gmail.com>
2022-04-12 22:03:44 -0500

    Restructuring for deployment

--- server/user/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from app import app # import Flask instance from app.py
-from user.models import User
-
-
-@app.route('/user/signup', methods=["GET"])
-def signup():
-  return User().signup()
